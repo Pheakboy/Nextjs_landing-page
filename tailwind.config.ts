3bdbdd130fd90f18fd3b8c1151f5fb2b736d5249
Sok Sothearith <thearith@Theariths-MacBook-Pro.local>
2025-01-27 11:01:09 +0700

    update tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          main: '#252E45',
+          // light: '',
+          // dark: '',
+        },
       },
+      textColor: {
+        black: { primary: '#FFFFFF', secondary: '#72777A' },
+      },
+      backgroundColor: {
+        primary: '#252E45',
+      },
+      backgroundImage: {},
     },
   },
   plugins: [],
